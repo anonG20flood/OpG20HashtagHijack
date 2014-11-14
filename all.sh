f8488c81aa30086f7d7a9645d82af91d5cca5010
Anonymous G20 Hashtag Hijack <anon_G20_flood@hotmail.com>
2014-11-14 17:14:54 +1100

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,263 +1,79 @@
 #!/bin/bash
-r=$(( $r + 1 ))
+t update 'Here commence the #OpG20HashtagHijack #G20 #Brisbane'
+sleep 30
 t update 'The only thing Orwell got wrong was the year #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'We can protect overseas dignitaries but not our disadvantaged, homeless and indigenous? #G20'
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'Homelessness is at an all time high, nobody cares about a few hundred elite #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-r=$(( $r + 1 ))
+t update 'Download me on Github https://github.com/anonG20flood/OpG20HashtagHijack  #G20 #Brisbane'
+sleep 30
 t update 'We can pour millions into foreign elite but not our own homeless citizens? #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'The wellbeing of our fellow Australians should be put before a few hundred elite. #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'Not my world leaders. #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-r=$(( $r + 1 ))
-t update "1000\'s go to bed tonight hungry, scared and cold while Abbott accomodates a few 100 elite #G20 #Brisbane #G20Brisbane #Auspol"
+t update "1000's go to bed tonight hungry, scared and cold while Abbott accomodates a few 100 elite #G20 #Brisbane #G20Brisbane #Auspol"
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
-t update "It\'s apparant Abbott cares more for his elite mates then he does for the people of Australia #G20 #Brisbane #G20Brisbane #Auspol"
+t update "It's apparant Abbott cares more for his elite mates then he does for the people of Australia #G20 #Brisbane #G20Brisbane #Auspol"
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
-t update "They claim I\'m violent because I refuse to remain silent #G20 #Brisbane #G20Brisbane #Auspol"
+t update "They claim I'm violent because I refuse to remain silent #G20 #Brisbane #G20Brisbane #Auspol"
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'No government can ever give you freedom #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
+t update 'Download me on Github https://github.com/anonG20flood/OpG20HashtagHijack  #G20 #Brisbane'
+sleep 30
+t update 'Download me on Github https://github.com/anonG20flood/OpG20HashtagHijack  #G20 #Brisbane'
+sleep 30
 t update 'Real anarchists throw ideas not bombs #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash psa02.sh
-#!/bin/bash
-r=$(( $r + 1 ))
 t update "Saying we can't live without the government is like saying animals can't survive without farms #G20 #Brisbane #G20Brisbane #Auspol"
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'We need qualified mental health professionals in our schools, Not More Chaplains #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'Respect existence or expect resistence #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'If the climate was a bank we would of already saved it #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'Migration is a natural right for all humans #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'I see humans but no humanity #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
+t update '#OpG20HashtagHijack shall continue - I am a TwitterBot - #G20 #Brisbane'
+sleep 30
 t update 'Is it a crime to fight for what is mine? #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
 t update 'Fight for your world not your country #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
-t update "I\'m sick of old men dreaming up wars for young men to die in #G20 #Brisbane #G20Brisbane #Auspol"
+t update "I'm sick of old men dreaming up wars for young men to die in #G20 #Brisbane #G20Brisbane #Auspol"
 sleep 30
-echo $r
-bash $r.sh
-
-#!/bin/bash
-r=$(( $r + 1 ))
-t update "I\'m just a machine, doing as I\'m told! #G20 #Brisbane #G20Brisbane #Auspol"
+t update "I'm just a machine, doing as I'm told! #G20 #Brisbane #G20Brisbane #Auspol"
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-r=$(( $r + 1 ))
 t update " #G20 #Brisbane #G20Brisbane #Auspol"
 sleep 30
-echo $r
-bash psa03.sh
-#!/bin/bash
-# r=$(( $RANDOM % 10 + 20 ))
-r=$(( $r + 1 ))
 t update 'ACAB. Cops. Protectors of the rich, Hunters of the poor #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-# r=$(( $RANDOM % 10 + 30 ))
-r=$(( $r + 1 ))
 t update 'ACAB. Cops only care for their next paycheck #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-# r=$(( $RANDOM % 10 + 40 ))
-r=$(( $r + 1 ))
 t update 'Police everywhere, Justice nowhere #G20 #Brisbane #G20Brisbane #Auspol'
 sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-# r=$(( $RANDOM % 10 + 50 ))
-r=$(( $r + 1 ))
-t update "Badges don\'t grant extra rights #G20 #Brisbane #G20Brisbane #Auspol"
-sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-# r=$(( $RANDOM % 10 + 60 ))
-r=$(( $r + 1 ))
-t update 'Nothing says democracy like being surronded by cops at a peaceful demostration #G20 #Brisbane #G20Brisbane #Auspol'
-sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-# r=$(( $RANDOM % 10 + 70 ))
-r=$(( $r + 1 ))
-t update '"To serve and protect - The ruling class" #G20 #Brisbane #G20Brisbane #Auspol'
-sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-# r=$(( $RANDOM % 10 + 80 ))
-r=$(( $r + 1 ))
-t update 'The real crime is knowing cannabis can help others, but doing nothing #G20 #Brisbane #G20Brisbane #Auspol'
-sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-# r=$(( $RANDOM % 10 + 90 ))
-r=$(( $r + 1 ))
-t update 'No recorded deaths from cannabis ever #G20 #Brisbane #G20Brisbane #Auspol'
-sleep 30
-echo $r
-bash psa01.sh
-           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-                   Version 2, December 2004
-
-Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
-
-Everyone is permitted to copy and distribute verbatim or modified
-copies of this license document, and changing it is allowed as long
-as the name is changed.
-
-           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
- 0. You just DO WHAT THE FUCK YOU WANT TO.
-#!/bin/bash
-r=""
-t update "Let\'s start this all again next hour! #G20 #G20Brisbane #Auspol"
-sleep 30
-echo "That's all folks!"
-
-#!/bin/bash
-# r=$(( $RANDOM % 10 ))
-r=$(( 1 - 1 ))
-t update 'Download me on Github https:\/\/github.com\/anonG20flood\/OpG20HashtagHijack  #G20 #Brisbane'
-sleep 30
-echo "PSA01"
-bash $r.sh
-#!/bin/bash
-# r=$(( $RANDOM % 10 ))
-# r=$(( 1 - 1 ))
 t update "I am a TwitterBot, #FollowingFriday @AnonymousNewsHQ #G20 #Brisbane"
 sleep 30
-echo "PSA02"
-bash $r.sh
-#!/bin/bash
-r=$(( 1 - 1 ))
-t update '#OpG20HashtagHijack shall continue - I am a TwitterBot - #G20 #Brisbane'
+t update "Badges don't grant extra rights #G20 #Brisbane #G20Brisbane #Auspol"
+sleep 30
+t update 'Nothing says democracy like being surronded by cops at a peaceful demostration #G20 #Brisbane #G20Brisbane #Auspol'
+sleep 30
+t update '"To serve and protect - The ruling class" #G20 #Brisbane #G20Brisbane #Auspol'
+sleep 30
+t update 'The real crime is knowing cannabis can help others, but doing nothing #G20 #Brisbane #G20Brisbane #Auspol'
+sleep 30
+t update 'No recorded deaths from cannabis ever #G20 #Brisbane #G20Brisbane #Auspol'
+sleep 30
+t update "Let's start this all again next hour! #G20 #G20Brisbane #Auspol"
+sleep 30
+t update 'Download me on Github https://github.com/anonG20flood/OpG20HashtagHijack  #G20 #Brisbane'
 sleep 30
 t update 'I shall be back after a few minutes... I need to catch my breath #G20 '
 sleep 180
-echo "PSA03"
-bash $r.sh
-\#OpG20HashTagHijack
-====================
-
-Bash scripts called in sequential order to flood \#G20 and \#Brisbane hashtags on Twitter
-
-To start, this requires T https://github.com/sferik/t to be installed.
-
-Once setup, commence flooding with `bash start.sh`
-#!/bin/bash
-# r=$(( $RANDOM % 10 ))
-r=$(( 1 - 1 ))
-t update 'Here commence the #OpG20HashtagHijack #G20 #Brisbane'
-sleep 30
-echo $r
-bash $r.sh
-#!/bin/bash
-# r=$(( $RANDOM % 10 ))
-r=$(( 1 - 1 ))
-t update 'Here commence the #OpG20HashtagHijack #G20 #Brisbane'
-sleep 30
-echo $r
-bash $r.sh
