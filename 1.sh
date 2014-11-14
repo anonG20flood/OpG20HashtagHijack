fa6b3068c55d8e7a7f7de2c77ad890ca17ba3fad
Danijel J <admin@danijelj.com>
2014-11-14 17:07:21 +1100

    All created

--- 1.sh
+++ 1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 r=$(( $r + 1 ))
-t update "Saying we can\'t live without the government is like saying animals can\'t survive without farms #G20 #Brisbane #G20Brisbane #Auspol"
+t update "Saying we can't live without the government is like saying animals can't survive without farms #G20 #Brisbane #G20Brisbane #Auspol"
 sleep 30
 echo $r
 bash $r.sh
